4b83b41cc4f024ecd77783387d72f208a138d490
Vlad Korzun <vladyslavkorzun@gmail.com>
2017-12-14 11:17:15 +0200

    Added SkeletonView header file

---
+++ Sources/SkeletonView.h
@@ -0,0 +1,15 @@
+//
+//  SkeletonView.h
+//  SkeletonView
+//
+//  Created by Vlad Korzun on 12/14/17.
+//  Copyright © 2017 SkeletonView. All rights reserved.
+//
+
+/// Project version number for SkeletonView.
+extern double SkeletonViewVersionNumber;
+
+/// Project version string for SkeletonView.
+extern const unsigned char SkeletonViewVersionString[];
+
+@import Foundation;
